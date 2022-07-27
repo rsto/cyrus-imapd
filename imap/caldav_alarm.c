c1e00b11d912f11c92834ebec9f7f0a3b7742759
Robert Stepanek <rsto@fastmailteam.com>
2022-07-27 22:08:53 +0200

    jmap_calendar: do not redefine the same annotation name multiple times
    Signed-off-by: Robert Stepanek <rsto@fastmailteam.com>

--- imap/caldav_alarm.c
+++ imap/caldav_alarm.c
@@ -53,6 +53,7 @@
 #include "caldav_alarm.h"
 #include "caldav_db.h"
 #include "calsched_support.h"
+#include "caldav_util.h"
 #include "cyrusdb.h"
 #include "httpd.h"
 #include "http_dav.h"
@@ -995,15 +996,12 @@ static time_t process_alarms(const char *mboxname, uint32_t imap_uid,
 
     /* Add default alarms */
     if (icalcomponent_read_usedefaultalerts_value(ical) > 0) {
-        static const char *withtime_annot =
-            DAV_ANNOT_NS "<" XML_NS_CALDAV ">default-alarm-vevent-datetime";
-        static const char *withdate_annot =
-            DAV_ANNOT_NS "<" XML_NS_CALDAV ">default-alarm-vevent-date";
-
         icalcomponent *withtime =
-            read_calendar_icalalarms(mboxname, userid, withtime_annot);;
+            read_calendar_icalalarms(mboxname, userid,
+                    CALDAV_DEFAULTALARMS_ANNOT_WITHTIME);
         icalcomponent *withdate =
-            read_calendar_icalalarms(mboxname, userid, withdate_annot);
+            read_calendar_icalalarms(mboxname, userid,
+                    CALDAV_DEFAULTALARMS_ANNOT_WITHDATE);
 
         if (withtime || withdate) {
             myical = icalcomponent_clone(ical);
