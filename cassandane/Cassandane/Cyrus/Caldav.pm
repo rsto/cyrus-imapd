b2c512026413196cd41eb86696dcb06659d32873
ellie timoney <ellie@fastmail.com>
2024-07-05 13:41:51 +1000

    FIXUP orig: don't set defaultdomain
    not sure why this was added but it breaks half the existing
    Caldav tests.  if we do need it for imip_quote_cn, we'll just
    set it for that one test

--- cassandane/Cassandane/Cyrus/Caldav.pm
+++ cassandane/Cassandane/Cyrus/Caldav.pm
@@ -111,7 +111,6 @@ sub new
     $config->set(caldav_realm => 'Cassandane');
     $config->set(httpmodules => 'caldav');
     $config->set(calendar_user_address_set => 'example.com');
-    $config->set(defaultdomain => 'example.com');
     $config->set(httpallowcompress => 'no');
     $config->set(caldav_historical_age => -1);
     $config->set(icalendar_max_size => 100000);
