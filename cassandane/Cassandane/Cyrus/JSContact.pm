16a9089e7fc606d6b6b0b99c2a6b590c2133b6cc
Robert Stepanek <rsto@fastmailteam.com>
2026-07-09 15:28:55 +0200

    XXX cass: snapshot rfc9555bis test

--- cassandane/Cassandane/Cyrus/JSContact.pm
+++ cassandane/Cassandane/Cyrus/JSContact.pm
@@ -2,12 +2,36 @@ package Cassandane::Cyrus::JSContact;
 use strict;
 use warnings;
 
-use base qw(Cassandane::Cyrus::TestCase);
+use HTTP::Tiny;
 
-sub new
-{
-    my $class = shift;
-    return $class->SUPER::new({}, @_);
+use base qw(Cassandane::Cyrus::TestCase);
+use Cassandane::Util::Log;
+
+sub new {
+    my ($class, @args) = @_;
+    my $config = Cassandane::Config->default()->clone();
+
+    $config->set(
+        caldav_realm                => 'Cassandane',
+        caldav_historical_age       => -1,
+        conversations               => 'yes',
+        httpmodules                 => 'convert',
+        httpallowcompress           => 'no',
+        jmap_nonstandard_extensions => 'yes',
+        defaultdomain               => 'example.com'
+    );
+
+    my $self = $class->SUPER::new(
+        {
+            config   => $config,
+            jmap     => 1,
+            services => ['http'],
+        },
+        @args
+    );
+
+    $self->needs('component', 'jmap');
+    return $self;
 }
 
 use Cassandane::Tiny::Loader;
